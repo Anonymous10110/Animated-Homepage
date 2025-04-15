68c2a688db59ec3107d11b7da27022084f4b6e5b
Anonymous <106383643+Anonymous10110@users.noreply.github.com>
2025-04-15 12:37:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+  function changeContent() {
+      const title = document.getElementById('title');
+      const desc = document.getElementById('desc');
+
+      title.innerText = "Let's Build Something Awesome";
+      desc.innerText = "Experience smooth design and seamless interaction.";
+    }
